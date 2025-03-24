d8f29f95c228a8c9bf003a183e7146f5c91de7af
Stubh Lal <77234323+Artshouldterrify@users.noreply.github.com>
2025-03-24 22:25:10 +0530

    Update Inference.py

--- recommender_code/Inference.py
+++ recommender_code/Inference.py
@@ -18,8 +18,8 @@ d.load_model('hackathon_xgboost_model_3.model')
 xgb_models = [a,b,c,d]
 pd.set_option('display.max_columns', None)
 df_pivot = pd.read_csv("pivot.csv", index_col=False)
-df_pivot_loan = pd.read_csv("pivot_loan_info.csv")
-df_pivot_loan = pd.get_dummies(df_pivot_loan, columns=['Gender','Marital_Status','Dependents','Education','Employment_Status','Occupation_Type','Residential_Status'], dtype='float32')
+df_pivot_l = pd.read_csv("pivot_loan_info.csv")
+df_pivot_loan = pd.get_dummies(df_pivot_l, columns=['Gender','Marital_Status','Dependents','Education','Employment_Status','Occupation_Type','Residential_Status'], dtype='float32')
 df_pivot_loan = df_pivot_loan.drop(columns="Loan_Purpose")
 df_pivot = df_pivot.drop(columns=["Unnamed: 0"])
 
@@ -70,7 +70,15 @@ def infer(cust_id=15906):
         loan_suggestions.append(xgboost_dict["Loan_Purpose"])
     return ranked_suggestions, loan_suggestions
 
+# inference function for all the data we have on customer.
+def customer_info(cust_id=15906):
+    cust_id = int(cust_id)
+    row_1 = df_pivot_l[df_pivot_l["Applicant_ID"] == cust_id].drop(columns=["Applicant_ID","Unnamed: 0","Loan_Approval_Status"])
+    print(row_1)
+    return row_1.to_dict()
+
 # main
-c_id = 15906
-r_suggestions, l_suggestions = infer(c_id)
-print(r_suggestions, l_suggestions)
+# c_id = 15906
+# customer_info(c_id)
+# r_suggestions, l_suggestions = infer(c_id)
+# print(r_suggestions, l_suggestions)
